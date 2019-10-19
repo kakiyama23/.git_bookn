9e37998d10f2b60fdfad39375cdec4d9fbeab06a
EC2 Default User <ec2-user@ip-172-31-34-32.ap-northeast-1.compute.internal>
2019-10-19 23:15:08 +0000

    second commit

--- scrapin.rb
+++ scrapin.rb
@@ -1,15 +1,5 @@
 require 'mechanize'
-    agent = Mechanize.new
-    page = agent.get("http://kc.kodansha.co.jp/new_comics")
-    titles = page.search('.title')
-    titles.each do |title|
-        puts title.inner_text
-    end
-    
-    images = page.search('.itemImg img')
-    images.each do |image_url|
-        puts image_url.get_attribute('src')
-    end
-    
-    
- 
+  agent = Mechanize.new
+  page = agent.get("http://kc.kodansha.co.jp/product?item=0000323878")
+  open_date = page.at('.spaceSp tr td').inner_text if page.at('.spaceSp tr td')
+  puts open_date
